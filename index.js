49d7f604defbdd5380dfbda45cfb637c3094642a
edwagbb <116136084+edwagbb@users.noreply.github.com>
2025-05-08 20:08:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,79 +4,101 @@ var CLEAR_TIMEOUT = 10 * 60 * 1000;
 var FILE_MODE = import.meta.url.indexOf("file:") === 0 || false;
 var WORKER_LIST = {}
 setInterval(() => {
-    for (let i in WORKER_LIST) {
-        if ((Date.now() - WORKER_LIST[i].lasttime) > CLEAR_TIMEOUT) {
-            console.log("🔚 时间差不多咯，结束 " + i)
-            WORKER_LIST[i].worker.terminate()
-            delete WORKER_LIST[i]
-        }
+  for (let i in WORKER_LIST) {
+    if ((Date.now() - WORKER_LIST[i].lasttime) > CLEAR_TIMEOUT) {
+      console.log("🔚 时间差不多咯，结束 " + i)
+      WORKER_LIST[i].worker.terminate()
+      delete WORKER_LIST[i]
     }
+  }
 }, 60 * 1000);
 
 
 try {
-    Deno.serve({ port: Deno.env.get("PORT") || 8000 }, handler);
-} catch (e) {  }
+  Deno.serve({ port: Deno.env.get("PORT") || 8000 }, handler);
+} catch (e) { }
 
 
 async function handler(req) {
-    try {
-        var host = req.url.match(/^https?:\/\/([^\/]+)/)[1];
-        var js = host.split('.').shift();
-        var handler_js = import.meta.url.match(/^([^\?]+)/)[1];
-        if (js.indexOf("re-") === 0) {
-            js = js.slice(3)
+  try {
+    var host = req.url.match(/^https?:\/\/([^\/]+)/)[1];
+    var js = host.split('.').shift();
+    var handler_js = import.meta.url.match(/^([^\?]+)/)[1];
+    if (js.indexOf("re-") === 0) {
+      js = js.slice(3)
 
-            handler_js = handler_js.slice(0, handler_js.lastIndexOf("/")) + "/" + js;
+      handler_js = handler_js.slice(0, handler_js.lastIndexOf("/")) + "/" + js;
 
-            if (WORKER_LIST[handler_js]) {
-                console.log("🔄 重新加载 " + handler_js)
-                WORKER_LIST[handler_js].worker.terminate()
-                delete WORKER_LIST[handler_js]
-            }
-        }
-        handler_js = handler_js.slice(0, handler_js.lastIndexOf("/")) + "/" + js;
-        if (!WORKER_LIST[handler_js]) {
-            var js_code = "";
-            if (FILE_MODE) {
-                try {
-                    js_code = await Deno.readTextFile(handler_js.replace(/^file:\/\/\//, "") + ".js");
-                } catch (e) {
-                    js_code = await Deno.readTextFile(handler_js.replace(/^file:\/\/\//, "") + ".ts");
-                }
-            } else {
-                try {
-                    js_code = await fetch(handler_js + ".js");
-                    js_code = await js_code.text();
-                } catch (e) {
-                    js_code = await fetch(handler_js + ".ts");
-                    js_code = await js_code.text();
-                }
-            }
-
-            var { worker, port } = await ServeInWorker(js_code, PORT_RANGE, host)
-
-            WORKER_LIST[handler_js] = {
-                code: js_code,
-                url: "http://localhost:" + port,
-                worker,
-                lasttime: Date.now()
-            }
-
-        }
-
-        WORKER_LIST[handler_js].lasttime = Date.now()
-        return fetch(req.url.replace(/^https?:\/\/[^\/]+/, WORKER_LIST[handler_js].url), req)
-
-
-    } catch (e) {
-        return new Response(e.message, { status: 500 })
+      if (WORKER_LIST[handler_js]) {
+        console.log("🔄 重新加载 " + handler_js)
+        WORKER_LIST[handler_js].worker.terminate()
+        delete WORKER_LIST[handler_js]
+      }
     }
+    handler_js = handler_js.slice(0, handler_js.lastIndexOf("/")) + "/" + js;
+
+    if (!WORKER_LIST[handler_js]) {
+      try {
+        WORKER_LIST[handler_js] = { resolve: [] }
+        var js_code = "";
+        if (FILE_MODE) {
+          try {
+            js_code = await Deno.readTextFile(handler_js.replace(/^file:\/\/\//, "") + ".js");
+          } catch (e) {
+            js_code = await Deno.readTextFile(handler_js.replace(/^file:\/\/\//, "") + ".ts");
+          }
+        } else {
+          try {
+            js_code = await fetch(handler_js + ".js");
+            js_code = await js_code.text();
+          } catch (e) {
+            js_code = await fetch(handler_js + ".ts");
+            js_code = await js_code.text();
+          }
+        }
+
+        var { worker, port } = await ServeInWorker(js_code, PORT_RANGE, host)
+
+        WORKER_LIST[handler_js] = {
+          code: js_code,
+          url: "http://localhost:" + port,
+          worker,
+          lasttime: Date.now()
+        }
+      } catch (e) {
+        //delete WORKER_LIST[handler_js]
+      } finally {
+        if (WORKER_LIST[handler_js]?.resolve) {
+          for (let resolve of WORKER_LIST[handler_js].resolve) {
+            try {
+              resolve()
+            } catch (e) { }
+          }
+          delete WORKER_LIST[handler_js].resolve
+        }
+        if (!WORKER_LIST[handler_js]?.url) delete WORKER_LIST[handler_js];
+      }
+    } else if (WORKER_LIST[handler_js].resolve) {
+      await new Promise(r => {
+        WORKER_LIST.resolve.push(r)
+      })
+    }
+
+    if (WORKER_LIST[handler_js]?.url) {
+      WORKER_LIST[handler_js].lasttime = Date.now()
+      return fetch(req.url.replace(/^https?:\/\/[^\/]+/, WORKER_LIST[handler_js].url), req)
+    } else {
+      return new Response("No handler!")
+    }
+
+  } catch (e) {
+    return new Response(e.message, { status: 500 })
+  }
 }
 
 async function ServeInWorker(jsCode, PORT_RANGE, host) {
-    // 构建Worker脚本，包装提供的代码
-    const workerScript = `
+  // 构建Worker脚本，包装提供的代码
+  const workerScript = `
 //Hook Deno.serve 实现固定端口
 ;(()=>{
 const originalServe = Deno.serve;
@@ -197,57 +219,57 @@ ${jsCode}
       };*/
     `;
 
-    // 创建data URL作为Worker脚本源
-    const dataUrl = `data:application/typescript;charset=utf-8,${encodeURIComponent(workerScript)}`;
+  // 创建data URL作为Worker脚本源
+  const dataUrl = `data:application/typescript;charset=utf-8,${encodeURIComponent(workerScript)}`;
 
-    // 创建Worker并设置严格的权限
-    const worker = new Worker(dataUrl, {
-        type: "module",
-        deno: {
-            namespace: true, // 不允许访问Deno命名空间
-            permissions: {
-                net: true,     // 允许网络访问
-                read: true,    // 不允许文件读取
-                write: true,   // 不允许文件写入
-                env: false,     // 不允许环境变量访问
-                hrtime: true,  // 允许高精度时间
-                run: false      // 不允许运行子进程
-            }
-        }
+  // 创建Worker并设置严格的权限
+  const worker = new Worker(dataUrl, {
+    type: "module",
+    deno: {
+      namespace: true, // 不允许访问Deno命名空间
+      permissions: {
+        net: true,     // 允许网络访问
+        read: true,    // 不允许文件读取
+        write: true,   // 不允许文件写入
+        env: false,     // 不允许环境变量访问
+        hrtime: true,  // 允许高精度时间
+        run: false      // 不允许运行子进程
+      }
+    }
+  });
+
+  return new Promise((resolve, reject) => {
+    // 设置超时处理
+    var timeout = 3000
+    const timeoutId = setTimeout(() => {
+      worker.terminate();
+      reject(new Error(`Serve timed out after ${timeout}ms`));
+    }, timeout);
+
+    // 处理Worker发送的消息
+    worker.onmessage = (e) => {
+      clearTimeout(timeoutId);
+
+      if (e.data.success) {
+        resolve({ worker, port: e.data.port })
+      } else {
+        const error = new Error(e.data.error.message);
+        error.name = e.data.error.name;
+        error.stack = e.data.error.stack;
+        reject(error);
+      }
+    };
+
+    worker.addEventListener("error", (event) => {
+      // console.error("工作线程错误事件:", event);
+      clearTimeout(timeoutId);
+      worker.terminate(); // 立即释放资源
+      reject(new Error(`Worker error`));
+
+      event.preventDefault();
     });
 
-    return new Promise((resolve, reject) => {
-        // 设置超时处理
-        var timeout = 3000
-        const timeoutId = setTimeout(() => {
-            worker.terminate();
-            reject(new Error(`Serve timed out after ${timeout}ms`));
-        }, timeout);
-
-        // 处理Worker发送的消息
-        worker.onmessage = (e) => {
-            clearTimeout(timeoutId);
-
-            if (e.data.success) {
-                resolve({ worker, port: e.data.port })
-            } else {
-                const error = new Error(e.data.error.message);
-                error.name = e.data.error.name;
-                error.stack = e.data.error.stack;
-                reject(error);
-            }
-        };
-
-        worker.addEventListener("error", (event) => {
-           // console.error("工作线程错误事件:", event);
-           clearTimeout(timeoutId);
-           worker.terminate(); // 立即释放资源
-           reject(new Error(`Worker error`));
-
-            event.preventDefault();
-          });
-       
-    });
+  });
 }
 
 
